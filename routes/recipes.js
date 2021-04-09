787c0bc47d019f948ecae69396e8d0f3189c097b
Chantel Trainer <ctrainer@uw.edu>
2021-04-09 14:46:39 -0700

    fixed a deprecated package and made changes for recipes endpoint

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,10 +1,8 @@
 const API_KEY = process.env.SPOONACULAR_API_KEY
 
-//express is the framework we're going to use to handle requests
 const express = require('express')
 
-//request module is needed to make a request to a web service
-const request = require('request')
+const fetch = require('node-fetch');
 
 var router = express.Router()
 
@@ -13,35 +11,30 @@ var router = express.Router()
 
 /**
  * @api {get} /One-call weather Request to access current, hourly, and daily weather forecast.
- * @apiName OneCallWeather
- * @apiGroup Weather
+ * @apiName GetRecipesByIngredients
+ * @apiGroup Recipes
  * 
- * @apiParam {String} lat, lon	(required)	Geographical coordinates (latitude, longitude)
- * @apiParam {String} unit (Required) Units of measurement. standard, metric and imperial units are available. 
- * 
- * @apiHeader {String} authorization JWT provided from Auth get
+ * @apiParam {String} ingredients (Required)	A comma-separated list of ingredients that the recipes should contain.
+ * @apiParam {String} ranking (Optional) Whether to maximize used ingredients (1) or minimize missing ingredients (2) first.
  * 
  * @apiError (400: Missing Parameters) {String} message "Missing required information"
  * 
- * @apiDescription This end point is for getting the current weather and weather forecast for a location.
+ * @apiDescription This end point is for getting a list of recipes based on the ingredients you have
  */ 
 router.get("/", (req, res) => {
+    console.log(req.params["ingredients"])
+    if(req.ingredients){
+        let myIngredients = req.ingredients.replace(" ", "+")
+        req.ranking ? myRanking = req.myRanking : myRanking = 1 
 
-    if(req.lat && req.long){
-        let url = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + req.lat 
-        + '&lon=' + req.long + '&exclude=minutely,alerts&units=' + req.units + '&appid=' + process.env.OPEN_WEATHER_API_KEY
+        let url = 'https://api.spoonacular.com/recipes/findByIngredients?apiKey=' + API_KEY + '&ranking=' + myRanking 
+        + '&limitlicense=true&ignorepantry=true&ingredients=' + myIngredients
         
         //When this web service gets a request, make a request to the external weather api
-        request(url, function (error, response, body) {
-            if (error) {
-                res.send(error)
-            } else {
-                var n = body.indexOf("{")
-                var nakidBody = body.substring(n - 1)
+        fetch(url)
+            .then(response => response.json())
+            .then(json => console.log(json))
     
-                res.send(nakidBody)
-            }
-        })
     } else {
         res.status(400).send({
             message: "Missing required information"
