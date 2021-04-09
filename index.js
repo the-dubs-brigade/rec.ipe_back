173d119e70638c0f4c420b8012857ca022bec8c4
Chantel Trainer <ctrainer@uw.edu>
2021-04-09 12:37:04 -0700

    Adding some basic structure to index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const express = require('express')
+const app = express()
+
+
+app.use('/recipes', require('./routes/recipes.js')) 
+
+app.use('/ingredients', require('./routes/ingredients.js'))
+  
+app.use('/conversions', require('./routes/conversions.js'))
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Server up and running on port: " + (process.env.PORT || 5000));
+});
