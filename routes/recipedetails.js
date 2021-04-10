109e30a2ddaa255a7adc3e9dd646bdd3fc7ac1ec
Chantel Trainer <ctrainer@uw.edu>
2021-04-10 14:43:00 -0700

    changes but rec card still not working

--- routes/recipedetails.js
+++ routes/recipedetails.js
@@ -3,6 +3,10 @@ const express = require('express')
 
 const fetch = require('node-fetch');
 
+var FormData = require('form-data');
+
+var fs = require('fs');
+
 const dotenv = require('dotenv');
 const { query } = require('express');
 dotenv.config();
@@ -20,16 +24,16 @@ var router = express.Router()
  * @apiError (400: Missing Parameters) {String} message "Missing required information"
  * 
  * @apiDescription This end point is for getting the details of a specific recipe by id.
- */ 
+ */
 router.get("/", (req, res) => {
     req.id = req.query.id;
 
-    if(req.id){
+    if (req.id) {
 
-        let myUrl = 'https://api.spoonacular.com/recipes/' + req.id + '/information?apiKey=' 
-            + process.env.SPOONACULAR_API_KEY 
+        let myUrl = 'https://api.spoonacular.com/recipes/' + req.id + '/information?apiKey='
+            + process.env.SPOONACULAR_API_KEY
         console.log(myUrl)
-        
+
         fetch(myUrl)
             .then(response => response.json())
             .then(json => {
@@ -37,7 +41,7 @@ router.get("/", (req, res) => {
                 res.send(json)
             })
             .catch(error => console.error('Error:', error))
-    
+
     } else {
         res.status(400).send({
             message: "Missing required information"
@@ -55,54 +59,99 @@ router.get("/", (req, res) => {
  * 
  * @apiError (400: Missing Parameters) {String} message "Missing required information"
  * 
- * @apiDescription This end point is for getting the details of a specific recipe by id.
- */ 
- router.get("/card", (req, res, next) => {
+ * @apiDescription This end point is for creating a recipe card for displaying
+ */
+router.get("/card", (req, res, next) => {
     req.id = req.query.id;
 
-    if(req.id){
-        let myUrl = 'https://api.spoonacular.com/recipes/' + req.id + '/information?apiKey=' 
-            + process.env.SPOONACULAR_API_KEY 
+    if (req.id) {
+        let myUrl = 'https://api.spoonacular.com/recipes/' + req.id + '/information?apiKey='
+            + process.env.SPOONACULAR_API_KEY
         console.log(myUrl)
-        
+
         fetch(myUrl)
             .then(response => response.json())
             .then(json => {
                 console.log(json)
-                res.title = json.title
-                res.image = json.image
-                console.log(json.extendedIngredients)
-                for (ing in json.extendedIngredients)
-                    res.ingredients += ing.amount + " " + ing.unit + " " + ing.name + "\n"
+                req.title = json.title
+                req.imageUrl = json.image
+                req.ingredients = ""
+                json.extendedIngredients.forEach(ing => {
+                    req.ingredients += ing.amount + " " + ing.unit + " " + ing.name + "\n"
+                })  
+                json.analyzedInstructions[0].steps.forEach(step => {
+                    req.instructions += step.step + "\n"
+                })
+                req.readyInMinutes = json.readyInMinutes
+                req.servings = json.servings
 
-                for (step in json.analyzedInstructions[0].steps)
-                    res.instructions += step.step + "\n"
-                
-                res.readyInMinutes = json.readyInMinutes
-                res.servings = json.servings
-                next()
+                fetch(req.imageUrl).then(response => {
+                    return response.buffer()
+                }).then(buffer => {
+                    console.log(buffer)
+                    res.buffer = buffer
+                    next()
+                })
+                // const response = await fetch(req.imageUrl);
+                // const buffer = await response.buffer();
+                // fs.writeFile(`./temp.jpg`, buffer, () => 
+                //   console.log('finished downloading image!'));
+               
             })
             .catch(error => console.error('Error:', error))
-    
+
     } else {
         res.status(400).send({
             message: "Missing required information"
         })
     }
 }, (req, res) => {
-    let queryString = '&title=' + res.title + '&image=' + res.image + '&ingredients=' + res.ingredients + '&instructions=' 
-    + res.instructions + '&readyInMinutes=' + res.readyInMinutes + '&servings=' + res.servings + '&mask=ellipseMask'
-    let encodedQueryString = encodeURI(queryString)
-    let myUrl = 'https://api.spoonacular.com/recipes/visualizeRecipe/?apiKey=' + process.env.SPOONACULAR_API_KEY 
-    + encodedQueryString
+    // const data = {
+    //     apiKey: process.env.SPOONACULAR_API_KEY,
+    //     title: req.title,
+    //     image: res.buffer,
+    //     ingredients: req.ingredients,
+    //     instructions: req.instructions,
+    //     readyInMinutes: req.readyInMinutes,
+    //     servings: req.servings,
+    //     mask: 'ellipseMask',
+    // };
 
-    console.log(myUrl)
+    // console.log(JSON.stringify(data))
 
-    // fetch(myUrl)
-    //     .then(response => response.json())
-    //     .then(json => {
-    //         res.send(json)
-    //     })
+
+    let url = 'https://api.spoonacular.com/recipes/visualizeRecipe?apiKey=' 
+        + process.env.SPOONACULAR_API_KEY
+    let myFormData = new FormData();
+    myFormData.append('apiKey', process.env.SPOONACULAR_API_KEY)
+    myFormData.append('title', req.title);
+    myFormData.append('image', res.buffer, {contentType: 'image/jpeg', filename: 'temp.jpg'});
+    myFormData.append('ingredients', req.ingredients);
+    myFormData.append('instructions', req.instructions);
+    myFormData.append('readyInMinutes', req.readyInMinutes);
+    myFormData.append('servings', req.servings);
+    myFormData.append('mask', 'ellipseMask');
+    myFormData.append('backgroundImage', 'none')
+    myFormData.append('author', 'none')
+    myFormData.append('backgroundColor', '#ffffff')
+    myFormData.append('fontColor', '#000000')
+    myFormData.append('source', 'spoonacular.com')
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'multipart/form-data',
+        },
+        body: myFormData,
+    })
+        .then(response => response.json())
+        .then(json => {
+            console.log(json);
+            res.send(json)
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
 })
 
 module.exports = router;
