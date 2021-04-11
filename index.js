968bd3693ce7f977f0cdd2f1c6e634320035e336
Chantel Trainer <ctrainer@uw.edu>
2021-04-10 20:09:12 -0700

    Cors fun

--- index.js
+++ index.js
@@ -4,13 +4,11 @@ const app = express()
 const dotenv = require('dotenv');
 dotenv.config();
 
-app.use('/recipesbyingredients', require('./routes/recipesbyingredients.js')) 
+app.use('/recipesbyingredients', cors(), require('./routes/recipesbyingredients.js')) 
 
-app.use('/recipedetails', require('./routes/recipedetails.js'))
+app.use('/recipedetails', cors(), require('./routes/recipedetails.js'))
 
-app.use('/random', require('./routes/randomrecipes.js'))
-
-app.use(cors())
+app.use('/random', cors(), require('./routes/randomrecipes.js'))
   
 // app.use('/conversions', require('./routes/conversions.js'))
 
@@ -18,10 +16,6 @@ console.log(`Your port is ${process.env.PORT}`); // 8626
 app.listen(process.env.PORT || 5000, () => {
     console.log("Server up and running on port: " + (process.env.PORT || 5000));
 }); 
-
-app.get('/products/:id', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
-})
 
 app.listen(80, function () {
   console.log('CORS-enabled web server listening on port 80')
