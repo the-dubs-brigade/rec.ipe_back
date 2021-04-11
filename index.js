d8d5f1643e2df630703e00b7d3c615157b4fa487
Chantel Trainer <ctrainer@uw.edu>
2021-04-10 22:03:15 -0700

    trying to fix issues with depoying

--- index.js
+++ index.js
@@ -18,10 +18,11 @@ app.use('/ingredients', require('./routes/ingredients.js'))
 // app.use('/conversions', require('./routes/conversions.js'))
 
 console.log(`Your port is ${process.env.PORT}`); 
+
 app.listen(process.env.PORT || 5000, () => {
     console.log("Server up and running on port: " + (process.env.PORT || 5000));
 }); 
 
-app.listen(80, function () {
-  console.log('CORS-enabled web server listening on port 80')
-})
+// app.listen(80, function () {
+//   console.log('CORS-enabled web server listening on port 80')
+// })
