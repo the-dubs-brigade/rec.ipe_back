527a9ccde2298166285ea9ef55004d78b6d6056d
Chantel Trainer <ctrainer@uw.edu>
2021-04-10 19:31:26 -0700

    fixing cors to allow all external sites

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 const express = require('express')
 const app = express()
 
+const dotenv = require('dotenv');
+dotenv.config();
+
 app.use('/recipesbyingredients', require('./routes/recipesbyingredients.js')) 
 
 app.use('/recipedetails', require('./routes/recipedetails.js'))
 
 app.use('/random', require('./routes/randomrecipes.js'))
+
+app.use(cors())
   
 // app.use('/conversions', require('./routes/conversions.js'))
 
-
-const dotenv = require('dotenv');
-dotenv.config();
 console.log(`Your port is ${process.env.PORT}`); // 8626
-
 app.listen(process.env.PORT || 5000, () => {
     console.log("Server up and running on port: " + (process.env.PORT || 5000));
 }); 
+
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
